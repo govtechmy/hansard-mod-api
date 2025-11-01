ba3db9b539dfb2830bee1eeabc7a4857e731bcb8
PyConqueror <wanaqim@gmail.com>
2025-11-01 16:10:17 +0800

    feat(authors): update controller

--- src/controllers/author.controller.ts
+++ src/controllers/author.controller.ts
@@ -3,12 +3,10 @@ import { createErrorResponse, createSuccessResponse } from "@/utils/response.uti
 
 export async function getAuthors(request: FastifyRequest, reply: FastifyReply) {
   try {
-    const [rows] = await request.server.sequelize.query(
-      `SELECT new_author_id, name, birth_year, ethnicity, sex
-       FROM api_author
-       ORDER BY new_author_id ASC`,
-    );
-    return reply.send(createSuccessResponse(rows, 200));
+    const Author = request.server.models.Author as any;
+    const rows = await Author.findAll({ order: [["new_author_id", "ASC"]] });
+    const json = rows.map((r: any) => (r?.toJSON?.() ?? r));
+    return reply.send(createSuccessResponse(json, 200));
   } catch (err: any) {
     return reply.code(400).send(createErrorResponse(err?.message ?? "Bad Request", "ERR_400", 400));
   }
