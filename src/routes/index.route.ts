c84ae91a49e19c6fdf670623d5f3ee25c333805e
PyConqueror <wanaqim@gmail.com>
2025-11-01 17:15:30 +0800

    chore(routes): register attendance, autocomplete, search, sitting, and speech routes

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -4,6 +4,11 @@ import { registerParliamentaryCycleRoutes } from "./parliamentary-cycle.route";
 import { registerAuthorRoutes } from "./author.route";
 import { registerAuthorHistoryRoutes } from "./author-history.route";
 import { registerCatalogueRoutes } from "./catalogue.route";
+import { registerSittingRoutes } from "./sitting.route";
+import { registerSearchRoutes } from "./search.route";
+import { registerAutocompleteRoutes } from "./autocomplete.route";
+import { registerAttendanceRoutes } from "./attendance.route";
+import { registerSpeechRoutes } from "./speech.route";
 
 export async function registerApiRoutes(app: FastifyInstance) {
   app.get("/health", { schema: { tags: ["System"], summary: "Healthcheck" } }, async () => {
@@ -19,6 +24,11 @@ export async function registerApiRoutes(app: FastifyInstance) {
   await registerAuthorRoutes(app);
   await registerAuthorHistoryRoutes(app);
   await registerCatalogueRoutes(app);
+  await registerSittingRoutes(app);
+  await registerSearchRoutes(app);
+  await registerAutocompleteRoutes(app);
+  await registerAttendanceRoutes(app);
+  await registerSpeechRoutes(app);
 }
 
 
